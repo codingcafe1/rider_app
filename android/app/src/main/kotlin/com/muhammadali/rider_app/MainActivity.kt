6dab7793ace93dfe5e691912c799372f7fc3e80d
unknown <alizeb875@gmail.com>
2021-08-07 23:51:42 -0700

    initial commit

---
+++ android/app/src/main/kotlin/com/muhammadali/rider_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.muhammadali.rider_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/AllScreens/mainscreen.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+
+
+class MainScreen extends StatefulWidget {
+  @override
+  _MainScreenState createState() => _MainScreenState();
+}
+
+
+
+class _MainScreenState extends State<MainScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Main Screen"),
+      ),
+    );
+  }
+}
